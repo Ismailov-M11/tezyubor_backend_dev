f55662a32a307b9d30002a30e65b39e859aa84bf
aknbdev <aknbdev@gmail.com>
2026-05-12 18:36:13 +0500

    fix: fixed variables not injecting issue

--- .github/scripts/export.sh
+++ .github/scripts/export.sh
@@ -3,7 +3,7 @@
 fetch() {
   curl -sf \
     -H "X-Vault-Token: ${VAULT_TOKEN}" \
-    "${{ secrets.VAULT_ADDR }}/v1/kv/data/$1" \
+    "${VAULT_ADDR}/v1/kv/data/$1" \
   | jq -r '
       .data.data
       | to_entries[]
